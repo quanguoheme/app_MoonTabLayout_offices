12e1027331ba6a94bc36da83552471e95c859fd1
quanguoheme <quanguoheme@qq.com>
2021-03-23 13:40:27 +0800

    简化

--- app/src/main/java/com/example/liuwangshu/mytablayout/TabLayoutActivity.java
+++ app/src/main/java/com/example/liuwangshu/mytablayout/TabLayoutActivity.java
@@ -6,6 +6,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 import java.util.ArrayList;
@@ -15,56 +17,21 @@ public class TabLayoutActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private ViewPager mViewPager;
     private TabLayout mTabLayout;
-
+    private List<Fruit> fruitList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab_layout);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-       // setSupportActionBar(toolbar);
-        mViewPager = (ViewPager) findViewById(R.id.viewpager);
-        initViewPager();
-    }
-
-    private void initViewPager() {
-        mTabLayout = (TabLayout) findViewById(R.id.tabs);
-        List<String> titles = new ArrayList<>();
-        titles.add("精选");
-        /*titles.add("体育");
-        titles.add("巴萨");
-        titles.add("购物");
-        titles.add("明星");
-        titles.add("视频");
-        titles.add("健康");
-        titles.add("励志");
-        titles.add("图文");
-        titles.add("本地");
-        titles.add("动漫");
-        titles.add("搞笑");
-        titles.add("精选");*/
-
-        for (int i = 0; i < titles.size(); i++) {
-            mTabLayout.addTab(mTabLayout.newTab().setText(titles.get(i)));
-        }
-        List<Fragment> fragments = new ArrayList<>();
-        for (int i = 0; i < titles.size(); i++) {
-            fragments.add(new ListFragment());
-        }
-        FragmentAdapter mFragmentAdapteradapter =
-                new FragmentAdapter(getSupportFragmentManager(), fragments, titles);
-        //给ViewPager设置适配器
-        mViewPager.setAdapter(mFragmentAdapteradapter);
-        //将TabLayout和ViewPager关联起来。
-        mTabLayout.setupWithViewPager(mViewPager);
-        //给TabLayout设置适配器
-        mTabLayout.setTabsFromPagerAdapter(mFragmentAdapteradapter);
-
         initFruits();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        FruitAdapter adapter = new FruitAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
     }
 
-    private List<Fruit> fruitList = new ArrayList<>();
     private void initFruits() {
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 1; i++) {
             Fruit apple = new Fruit("Apple", R.drawable.person_add);
             fruitList.add(apple);
             Fruit banana = new Fruit("Banana", R.drawable.person_delete);
